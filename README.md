205ae96fc15182b92d1fe0b26da538812f943613
saitejaMaydemakula <msaiteja665@gmail.com>
2025-04-07 23:15:01 +0530

    Initial commit SMS spam Classifier

---
+++ README.md
@@ -0,0 +1,13 @@
+# 📩 SMS Spam Classifier
+
+A simple machine learning web app that detects whether a given message is **Spam** or **Not Spam**, built using **Python, Streamlit, and scikit-learn**.
+
+---
+
+## 🚀 How to Run the Project
+
+### 1️⃣ Clone the repository
+
+```bash
+git clone https://github.com/your-username/sms-spam-classifier.git
+cd sms-spam-classifier
---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pickle
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+
+model = pickle.load(open('model.pkl', 'rb'))
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+nltk.download('punkt')
+nltk.download('stopwords')
+def lower_case(sentence):
+    sent=sentence.lower()
+    sent=nltk.word_tokenize(sent)
+    rem=[]
+    for i in sent:
+        if i.isalnum() and i not in stopwords.words('english'):
+            var=ps.stem(i)
+            rem.append(var)
+    return " ".join(rem)
+st.title('SMS Spam Classifier')
+input=st.text_area('Enter your message')
+if st.button('Classify'):
+    transform = lower_case(input)
+    vector=tfidf.transform([transform])
+    result=model.predict(vector)[0]
+    if result==1:
+        st.header('Spam')
+    else:
+        st.header('Not Spam')
